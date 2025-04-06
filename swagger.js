69e1305eac6dbc86c7f7ed53f8119552fe847bc9
Sydrickbissombolo <newtonbissomblo2@gmail.com>
2025-04-06 20:01:48 +0100

    render link back in swagger.json

--- swagger.js
+++ swagger.js
@@ -5,26 +5,26 @@ const doc = {
         title: 'Task Management API',
         description: 'API documentation for the Task Management API'
     },
-    host: 'localhost:8000',
-    schemes: ['http'],
-    // tags: [
-    //     {
-    //         name: "Categories",
-    //         description: "Endpoints for managing categories"
-    //     },
-    //     {
-    //         name: "Tasks",
-    //         description: "Endpoints for managing tasks"
-    //     },
-    //     {
-    //         name: "Users",
-    //         description: "Endpoints for managing users"
-    //     },
-    //     {
-    //         name: "Comments",
-    //         description: "Endpoints for managing comments"
-    //     }
-    // ]
+    host: 'cse341-final-project-2q9o.onrender.com',
+    schemes: ['https'],
+    tags: [
+        {
+            name: "Categories",
+            description: "Endpoints for managing categories"
+        },
+        {
+            name: "Tasks",
+            description: "Endpoints for managing tasks"
+        },
+        {
+            name: "Users",
+            description: "Endpoints for managing users"
+        },
+        {
+            name: "Comments",
+            description: "Endpoints for managing comments"
+        }
+    ]
 };
 
 const outputFile = './swagger.json';
